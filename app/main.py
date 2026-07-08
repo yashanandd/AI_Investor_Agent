bca9744cf9a34608a804ea3576053986a5125393
yashanandd <yash983747@gmail.com>
2026-07-08 14:41:35 +0530

    fixed cors policy

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://ai-investor-agent.onrender.com"
+        "https://ai-investor-agent-one.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
